0ccd6c6f74217621b99719c790d6c852c01202a6
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-03-19 09:48:19 +0100

    Add appending QR codes in the workflow

--- make_printable.sh
+++ make_printable.sh
@@ -4,4 +4,5 @@ makeindex main -s index_style.ist
 find sections -type f | xargs -n 1 sed -i "s@\\\hypertarget@\\\pagetarget@g"
 find sections -type f | xargs -n 1 sed -i "s@\\\hyperlink@\\\pagelink@g"
 sed -i "s@\\\include{sections/back_cover.tex}@\\\include{sections/index.tex}\\\include{sections/back_cover.tex}@g" main.tex
+sed -i -e '/% QR codes placeholder/{r .github/qr-codes.tex' -e 'd}' main.tex
 latexmk -pdf -silent "main"
