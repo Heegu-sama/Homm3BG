1ea6b478201bd0c53b9af6057402efb3a5966d3d
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-03-23 19:25:40 +0100

    Add Python script appending pages only to select hyperlinks

---
+++ .github/insert_printable_hyperlinks.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import fileinput
+import pathlib
+import re
+
+
+HYPERLINKS_TO_REPLACE = [
+    "AIrules",
+    "Ability",
+    "Combat",
+    "Defend",
+    "Difficulty",
+    "End",
+    "Endcombat",
+    "Level",
+    "Movement",
+    "Playerdecks",
+    "Quick",
+    "Trading",
+    "Walls",
+]
+
+def _list_tex_files(directory):
+    return pathlib.Path(f"./{directory}").glob("*.tex")
+
+
+def _replace_line(line):
+    for pattern in HYPERLINKS_TO_REPLACE:
+        match = re.search(f"hyperlink{{{pattern}", line)
+        if match:
+            line = line.replace(f"hyperlink{{{pattern}", f"pagelink{{{pattern}")
+    return line
+
+
+def _parse_file(file):
+    for line in file:
+        if "hyperlink" in line:
+            line = _replace_line(line)
+        print(line, end="")
+
+
+def update_files(directory):
+    for filename in _list_tex_files(directory):
+        with fileinput.FileInput(filename, inplace=True) as file:
+            _parse_file(file)
+
+
+if __name__ == "__main__":
+    update_files("sections")
