aa111ac1f66616955db4fa18053b675c04a8779d
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-04-17 17:14:30 +0200

    Update make_printable.sh script

--- make_printable.sh
+++ make_printable.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 makeindex main_en -s index_style.ist
-find sections -type f | xargs -n 1 sed -i "s@\\\hypertarget@\\\pagetarget@g"
+find sections -type f -execdir sed -i 's@\\hypertarget@\\pagetarget@g' '{}' +
 python .github/insert_printable_hyperlinks.py
-sed -i "s@\\\include{\\\sections/back_cover.tex}@\\\include{\\\sections/index.tex}\\\include{\\\sections/back_cover.tex}@g" metadata.tex
+sed -i 's@\\include{\\sections/back_cover.tex}@\\include{\\sections/index.tex}\\include{\\sections/back_cover.tex}@g' metadata.tex
 sed -i -e '/% QR codes placeholder/{r .github/qr-codes-en.tex' -e 'd}' metadata.tex
 latexmk -pdf -silent -shell-escape "main_en"
