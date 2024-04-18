dedd46a7f429bfedd170af269948f9282037bae6
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-04-18 20:07:52 +0200

    Update version and make_printable script with hash map

--- make_printable.sh
+++ make_printable.sh
@@ -5,7 +5,7 @@ PATHS=( ["en"]="sections" ["pl"]="sections/translated/pl" )
 
 makeindex main_en -s index_style.ist
 find sections -type f -execdir sed -i 's@\\hypertarget@\\pagetarget@g' '{}' +
-python .github/insert_printable_hyperlinks.py "sections"
+python .github/insert_printable_hyperlinks.py "${PATHS[$LANGUAGE]}"
 sed -i 's@\\include{\\sections/back_cover.tex}@\\include{\\sections/index.tex}\\include{\\sections/back_cover.tex}@g' metadata.tex
 sed -i -e "/% QR codes placeholder/{r .github/qr-codes-$LANGUAGE.tex" -e 'd}' metadata.tex
 latexmk -pdf -silent -shell-escape "main_${LANGUAGE}"
