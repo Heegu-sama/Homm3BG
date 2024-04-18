98b7dd43a5534b381e3ed30d798744d0143a6fc2
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-04-18 21:18:19 +0200

    Include Index page in printable only if it wasn't included already

--- make_printable.sh
+++ make_printable.sh
@@ -10,6 +10,9 @@ fi
 makeindex main_en -s index_style.ist
 find sections -type f -execdir sed -i 's@\\hypertarget@\\pagetarget@g' '{}' +
 python .github/insert_printable_hyperlinks.py "${SECTIONS}"
-sed -i 's@\\include{\\sections/back_cover.tex}@\\include{\\sections/index.tex}\\include{\\sections/back_cover.tex}@g' metadata.tex
+if [ $(grep -c "sections/index.tex" metadata.tex) -eq 0 ]
+then
+  sed -i 's@\\include{\\sections/back_cover.tex}@\\include{\\sections/index.tex}\\include{\\sections/back_cover.tex}@g' metadata.tex
+fi
 sed -i -e "/% QR codes placeholder/{r .github/qr-codes-$LANGUAGE.tex" -e 'd}' metadata.tex
 latexmk -pdf -silent -shell-escape "main_${LANGUAGE}"
