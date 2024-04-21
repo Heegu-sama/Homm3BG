6c89ff7ee79865a2f55f19e066c6fee6c050fedb
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-04-21 08:39:32 +0200

    Make build script platform independent

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     command=xdg-open;;
+    Darwin*)    command=open;;
+esac
+echo ${command}
+
 LANGUAGE=$1
 
 rm -f main_${LANGUAGE}.aux && \
   po4a --no-update po4a.cfg && \
   latexmk -pdf -shell-escape main_${LANGUAGE}.tex && \
-  xdg-open main_${LANGUAGE}.pdf &
+  ${command} main_${LANGUAGE}.pdf &
