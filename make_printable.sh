6870ac4cb8cb847084af74a3284051c41623151e
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-04-22 10:22:04 +0200

    Fix make_printable script

--- make_printable.sh
+++ make_printable.sh
@@ -7,7 +7,8 @@ else
   SECTIONS="sections/translated/${LANGUAGE}"
 fi
 
-makeindex main_en -s index_style.ist
+po4a --no-update po4a.cfg
+makeindex main_${LANGUAGE} -s index_style.ist
 find sections -type f -execdir sed -i 's@\\hypertarget@\\pagetarget@g' '{}' +
 python .github/insert_printable_hyperlinks.py "${SECTIONS}"
 if [ $(grep -c "sections/notes.tex" metadata.tex) -eq 1 ]
@@ -19,4 +20,4 @@ then
   sed -i 's@\\include{\\sections/back_cover.tex}@\\include{\\sections/index.tex}\\include{\\sections/back_cover.tex}@g' metadata.tex
 fi
 sed -i -e "/% QR codes placeholder/{r .github/qr-codes-$LANGUAGE.tex" -e 'd}' metadata.tex
-latexmk -pdf -silent -shell-escape "main_${LANGUAGE}"
+latexmk -pdf -shell-escape "main_${LANGUAGE}"
