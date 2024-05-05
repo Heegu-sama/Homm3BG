30013258bdfd490ac025949d6cf1a3a4c4592687
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-05-05 19:19:26 +0200

    Run compare script asynchronously

--- tools/compare_pages.sh
+++ tools/compare_pages.sh
@@ -11,14 +11,15 @@ else
 fi
 
 echo "Making images of ${FILE_TO_COMPARE}..."
-pdftoppm ${FILE_TO_COMPARE} aa -f ${FIRST_PAGE} -l ${LAST_PAGE} -png &
+pdftoppm ${FILE_TO_COMPARE} aa -f ${FIRST_PAGE} -l ${LAST_PAGE} -png -progress &
+sleep .5
 echo "Making images of main_${LANGUAGE}.pdf..."
-pdftoppm main_${LANGUAGE}.pdf bb -f ${FIRST_PAGE} -l ${LAST_PAGE} -png
+pdftoppm main_${LANGUAGE}.pdf bb -f ${FIRST_PAGE} -l ${LAST_PAGE} -png -progress
 
 for page in $(seq ${FIRST_PAGE} ${LAST_PAGE})
 do
   echo "Combining pages $(printf %02d $page)..."
-  montage *$(printf %02d $page).png -tile 2x1 -geometry +0+0 $(printf %02d $page).png
-  rm aa-$(printf %02d $page).png bb-$(printf %02d $page).png
+  montage *$(printf %02d $page).png -tile 2x1 -geometry +0+0 $(printf %02d $page).png && \
+  rm aa-$(printf %02d $page).png bb-$(printf %02d $page).png && sleep .3 &
 done
 echo "Done."
