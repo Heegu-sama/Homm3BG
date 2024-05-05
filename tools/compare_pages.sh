b478d70e7f291655597bc8d28c0b4e42e7e0ed95
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-05-05 19:39:36 +0200

    Adjust messages and progress

--- tools/compare_pages.sh
+++ tools/compare_pages.sh
@@ -10,16 +10,16 @@ else
   LAST_PAGE=$4
 fi
 
-echo "Making images of ${FILE_TO_COMPARE}..."
+echo "Making images of ${FILE_TO_COMPARE} and main_${LANGUAGE}.pdf..."
 pdftoppm ${FILE_TO_COMPARE} aa -f ${FIRST_PAGE} -l ${LAST_PAGE} -png -progress &
-sleep .5
-echo "Making images of main_${LANGUAGE}.pdf..."
-pdftoppm main_${LANGUAGE}.pdf bb -f ${FIRST_PAGE} -l ${LAST_PAGE} -png -progress
+sleep 1
+pdftoppm main_${LANGUAGE}.pdf bb -f ${FIRST_PAGE} -l ${LAST_PAGE} -png
 
 for page in $(seq ${FIRST_PAGE} ${LAST_PAGE})
 do
   echo "Combining pages $(printf %02d $page)..."
   montage *$(printf %02d $page).png -tile 2x1 -geometry +0+0 $(printf %02d $page).png && \
-  rm aa-$(printf %02d $page).png bb-$(printf %02d $page).png && sleep .3 &
+  rm aa-$(printf %02d $page).png bb-$(printf %02d $page).png &
+  sleep .2
 done
 echo "Done."
