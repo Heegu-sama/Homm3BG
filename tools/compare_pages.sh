79445d52d870529f2807d4545abd896224ff580f
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-05-06 09:36:26 +0200

    Introduce random dir

--- tools/compare_pages.sh
+++ tools/compare_pages.sh
@@ -10,16 +10,21 @@ else
   LAST_PAGE=$4
 fi
 
+RANDOM_DIR=$(cat /dev/random | tr -dc 'a-z' | fold -w 7 | head -n 1)
+mkdir /tmp/${RANDOM_DIR}
+
 echo "Making images of ${FILE_TO_COMPARE} and main_${LANGUAGE}.pdf..."
-pdftoppm ${FILE_TO_COMPARE} aa -f ${FIRST_PAGE} -l ${LAST_PAGE} -png -progress &
-pdftoppm main_${LANGUAGE}.pdf bb -f ${FIRST_PAGE} -l ${LAST_PAGE} -png -progress &
+pdftoppm ${FILE_TO_COMPARE} /tmp/${RANDOM_DIR}/aa -f ${FIRST_PAGE} -l ${LAST_PAGE} -png -progress &
+pdftoppm main_${LANGUAGE}.pdf /tmp/${RANDOM_DIR}/bb -f ${FIRST_PAGE} -l ${LAST_PAGE} -png -progress &
 wait
 
 for page in $(seq ${FIRST_PAGE} ${LAST_PAGE})
 do
   echo "Combining pages $(printf %02d $page)..."
-  montage *$(printf %02d $page).png -tile 2x1 -geometry +0+0 $(printf %02d $page).png && \
-  rm aa-$(printf %02d $page).png bb-$(printf %02d $page).png &
+  montage /tmp/${RANDOM_DIR}/*$(printf %02d $page).png -tile 2x1 -geometry +0+0 /tmp/${RANDOM_DIR}/$(printf %02d $page).png && \
+  rm /tmp/${RANDOM_DIR}/aa-$(printf %02d $page).png /tmp/${RANDOM_DIR}/bb-$(printf %02d $page).png &
 done
 wait
+mv /tmp/${RANDOM_DIR}/* .
+rm -rf /tmp/${RANDOM_DIR}/
 echo "Done."
