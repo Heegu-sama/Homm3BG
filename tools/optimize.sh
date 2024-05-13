ec4e27e370d9950816790ea7a92e6178a558cda9
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-05-13 21:42:09 +0200

    Don't reduce image quality

--- tools/optimize.sh
+++ tools/optimize.sh
@@ -6,6 +6,6 @@ LANGUAGE=$1
 gs -o main_${LANGUAGE}_optimized.pdf \
   -sDEVICE=pdfwrite \
   -dCompatibilityLevel=1.5 \
-  -dPDFSETTINGS=/printer \
+  -dPDFSETTINGS=/prepress \
   -dDetectDuplicateImages=true \
   main_${LANGUAGE}.pdf
