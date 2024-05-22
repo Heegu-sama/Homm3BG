ef3067c9193cc216ac61962bd3c3aff9db4ce95e
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-05-22 11:08:39 +0200

    Adjust dir/file version

--- tools/release.sh
+++ tools/release.sh
@@ -2,7 +2,8 @@
 
 LANGUAGE=$1
 
-VERSION=$(grep Version main_en.tex | grep -Po "\d.+$" | tr -d .)
+VERSION=$(grep Version main_en.tex | grep -Po "\d.+$")
+FILE_VERSION=$(echo "${VERSION}" | tr -d .)
 
 declare -A languages=(
   ["en"]="English"
@@ -21,7 +22,7 @@ tools/build.sh ${LANGUAGE} &> /dev/null
 echo "Please inspect the PDF file."
 echo "Optimizing digital build..."
 tools/optimize.sh ${LANGUAGE} &> /dev/null
-mv main_${LANGUAGE}_optimized.pdf release-${VERSION}/Heroes3_${languages[$LANGUAGE]}_Rules_Rewrite_${VERSION}.pdf
+mv main_${LANGUAGE}_optimized.pdf release-${VERSION}/Heroes3_${languages[$LANGUAGE]}_Rules_Rewrite_${FILE_VERSION}.pdf
 
 echo "Building printable version for ${languages[$LANGUAGE]}..."
 tools/make_printable.sh ${LANGUAGE} &> /dev/null
@@ -29,5 +30,5 @@ git restore metadata.tex sections/
 echo "Please inspect the PDF file."
 echo "Optimizing printable build..."
 tools/optimize.sh ${LANGUAGE} &> /dev/null
-mv main_${LANGUAGE}_optimized.pdf release-${VERSION}/Heroes3_${languages[$LANGUAGE]}_Rules_Rewrite_${VERSION}_Printable.pdf
+mv main_${LANGUAGE}_optimized.pdf release-${VERSION}/Heroes3_${languages[$LANGUAGE]}_Rules_Rewrite_${FILE_VERSION}_Printable.pdf
 echo "Done."
