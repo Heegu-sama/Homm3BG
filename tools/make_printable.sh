5f33584296a534f2a246ddd2fb75312e7e2f5d98
Alexey Sokolov <sokolov@google.com>
2024-05-23 23:51:36 +0100

    Simplify script

--- tools/make_printable.sh
+++ tools/make_printable.sh
@@ -1,29 +1,5 @@
 #!/usr/bin/env bash
-#
-case "$(uname -s)" in
-    Darwin*)    open=open;;
-    Linux*)     open=xdg-open;;
-esac
 
 LANGUAGE=$1
-if [[ ${LANGUAGE} == en ]]; then
-  SECTIONS=sections
-else
-  SECTIONS="sections/translated/${LANGUAGE}"
-fi
-
-case "${LANGUAGE}" in
-  ru|ua)
-    ENGINE=-pdflua
-    ;;
-  *)
-    ENGINE=-pdf
-    ;;
-esac
-
-po4a --no-update po4a.cfg
-
 export HOMM3_PRINTABLE=1 HOMM3_LANG=${LANGUAGE}
-latexmk ${ENGINE} -shell-escape main_${LANGUAGE}.tex
-
-${open} main_${LANGUAGE}.pdf &
+exec tools/build.sh "$@"
