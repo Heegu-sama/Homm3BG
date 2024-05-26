6ab7cb6e067434603d88bfb6a71b7423738e0332
Alexey Sokolov <alexey+github@asokolov.org>
2024-05-26 09:56:45 +0100

    Make all links pagelinks (#536)
    * Simplify printable
    
    * Make all links pagelinks
    
    * unpage paralysis
    
    * resize medusa in printable
    
    * Unpage trade table link
    
    * unpage difficulty table
    
    * Cleanup scripts
    
    * workflow
    
    * Simplify script
    
    * fuzzy overflown
    
    * unpage combat->difficulty table
    
    fuzzy "on back cover"
    
    * Update sections/heroes.tex
    
    Co-authored-by: qwrtln <qwrtln@users.noreply.github.com>
    
    * update po
    
    ---------
    
    Co-authored-by: qwrtln <qwrtln@users.noreply.github.com>

--- tools/make_printable.sh
+++ tools/make_printable.sh
@@ -1,34 +1,5 @@
 #!/usr/bin/env bash
-#
-case "$(uname -s)" in
-    Darwin*)    open=open;;
-    Linux*)     open=xdg-open;;
-esac
 
 LANGUAGE=$1
-if [[ ${LANGUAGE} == en ]]; then
-  SECTIONS=sections
-else
-  SECTIONS="sections/translated/${LANGUAGE}"
-fi
-
-case "${LANGUAGE}" in
-  ru|ua)
-    ENGINE=-pdflua
-    ;;
-  *)
-    ENGINE=-pdf
-    ;;
-esac
-
-po4a --no-update po4a.cfg
-
-find ${SECTIONS} -type f -execdir sed -i 's@\\hypertarget@\\pagetarget@g' '{}' +
-python .github/insert_printable_hyperlinks.py "${SECTIONS}"
-
-sed -i -e "/% QR codes placeholder/{r .github/qr-codes-$LANGUAGE.tex" -e 'd}' metadata.tex
-
 export HOMM3_PRINTABLE=1 HOMM3_LANG=${LANGUAGE}
-latexmk ${ENGINE} -shell-escape main_${LANGUAGE}.tex
-
-${open} main_${LANGUAGE}.pdf &
+exec tools/build.sh "$@"
