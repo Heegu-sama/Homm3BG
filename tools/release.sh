c87fff57acf3df920c36de354f70412d1d698ee4
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-11-15 14:51:09 +0100

    Update readme and release script

--- tools/release.sh
+++ tools/release.sh
@@ -24,24 +24,24 @@ echo "Building release ${VERSION} for ${languages[$LANGUAGE]}..."
 mkdir -p "release-${VERSION}"
 
 echo "Building digital version for ${languages[$LANGUAGE]}..."
-tools/build.sh "${LANGUAGE}" # &> /dev/null
+tools/build.sh "${LANGUAGE}" &> /dev/null
 echo "Please inspect the PDF file."
 echo "Optimizing digital build..."
-tools/optimize.sh "${LANGUAGE}" # &> /dev/null
+tools/optimize.sh "${LANGUAGE}" &> /dev/null
 mv "main_${LANGUAGE}_optimized.pdf" "release-${VERSION}/Heroes3_${languages[$LANGUAGE]}_Rules_Rewrite_${FILE_VERSION}.pdf"
 
 echo "Building printable version for ${languages[$LANGUAGE]}..."
 tools/build.sh "${LANGUAGE}" --printable &> /dev/null
 echo "Please inspect the PDF file."
 echo "Optimizing printable build..."
-tools/optimize.sh "${LANGUAGE}" cmyk &> /dev/null
+tools/optimize.sh "${LANGUAGE}" --cmyk &> /dev/null
 mv "main_${LANGUAGE}_optimized.pdf" "release-${VERSION}/Heroes3_${languages[$LANGUAGE]}_Rules_Rewrite_${FILE_VERSION}_Printable.pdf"
 
 echo "Building economy printable version for ${languages[$LANGUAGE]}..."
 tools/build.sh "${LANGUAGE}" --printable --no-bg &> /dev/null
 echo "Please inspect the PDF file."
 echo "Optimizing economy printable build..."
-tools/optimize.sh "${LANGUAGE}" cmyk &> /dev/null
+tools/optimize.sh "${LANGUAGE}" --mono &> /dev/null
 mv "main_${LANGUAGE}_optimized.pdf" "release-${VERSION}/Heroes3_${languages[$LANGUAGE]}_Rules_Rewrite_${FILE_VERSION}_Economy_Printable.pdf"
 
 echo "Done."
