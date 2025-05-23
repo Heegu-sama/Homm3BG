dec5ea5dde2b662e672a92eaf57c4eff0b6971ff
Jakub Klus <48711526+deep-jkl@users.noreply.github.com>
2025-05-23 20:40:14 +0000

    Translate all locations, improve tooling

--- tools/fuzzy_diff.sh
+++ tools/fuzzy_diff.sh
@@ -1,5 +1,7 @@
-msgattrib --only-fuzzy $1 > fuzzy.cs.po
-awk '/^#, fuzzy/ {print; found=1; next} found {print; if (/^msgstr/) exit}' fuzzy.cs.po > fuzzy.cs.po.first
-awk '/^#\|/,/^msgid/ {if ($0 ~ /^#\|/) print substr($0, 4)}' fuzzy.cs.po.first > old_translations.cs.po
-awk '/^msgid/,/^msgstr/ {print}' fuzzy.cs.po.first | head -n -1 > new_translations.cs.po
-diff -u --color old_translations.cs.po new_translations.cs.po
+MYTMPDIR="$(mktemp -d)"
+trap 'rm -rf -- "$MYTMPDIR"' EXIT
+msgattrib --only-fuzzy $1 > "$MYTMPDIR/fuzzy_only.po"
+awk '/^#, fuzzy/ {print; found=1; next} found {print; if (/^msgstr/) exit}' "$MYTMPDIR/fuzzy_only.po" > "$MYTMPDIR/fuzzy_first.po"
+awk '/^#\|/,/^msgid/ {if ($0 ~ /^#\|/) print substr($0, 4)}' "$MYTMPDIR/fuzzy_first.po" > "$MYTMPDIR/old.po"
+awk '/^msgid/,/^msgstr/ {print}' "$MYTMPDIR/fuzzy_first.po" | head -n -1 > "$MYTMPDIR/new.po"
+diff -u --color "$MYTMPDIR/old.po" "$MYTMPDIR/new.po"
